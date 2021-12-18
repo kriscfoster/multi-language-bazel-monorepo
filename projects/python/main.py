94c5569bbfbef711948820f6f8a4c8c22ae3b595
kriscfoster <krisfoster@live.ie>
2021-12-18 20:39:40 +0000

    good progress

--- projects/python/main.py
+++ projects/python/main.py
@@ -1,5 +1,17 @@
+from flask import Flask
+from random import randint
+
 from calculator.calculator import Calculator
 
-calc = Calculator()
-print("1 + 2 =", calc.add(1, 2))
-print("4 + 5 =", calc.add(4, 5))
+app = Flask(__name__)
+my_calculator = Calculator()
+
+@app.route('/')
+def hello():
+  num1 = randint(0, 100)
+  num2 = randint(0, 100)
+  message = "Did you know {} + {} = {}?".format(num1, num2, my_calculator.add(num1, num2))
+  return message
+
+if __name__ == '__main__':
+  app.run()
