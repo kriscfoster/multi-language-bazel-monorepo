e517e56f88296535fce2c66c347d105fbc113a57
kriscfoster <krisfoster@live.ie>
2022-03-28 07:12:31 +0100

    random number

--- projects/node_web/index.js
+++ projects/node_web/index.js
@@ -6,9 +6,15 @@ const calculator = new Calculator();
 const port = process.env.PORT | 8080;
 
 app.get('/', (_req, res) => {
-  res.send(`did you know that 1 + 2 = ${calculator.add(1, 2)}`);
+  const rand1 = getRandomInt(100);
+  const rand2 = getRandomInt(100);
+  res.send(`did you know that ${rand1} + ${rand2} = ${calculator.add(rand1, rand2)}`);
 });
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
