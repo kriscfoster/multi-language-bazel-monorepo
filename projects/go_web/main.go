17c170f631cf4ac02ece476aebbf1f3b5fa68ca1
kriscfoster <krisfoster@live.ie>
2022-06-28 21:20:32 +0100

    revert change

--- projects/go_web/main.go
+++ projects/go_web/main.go
@@ -11,7 +11,7 @@ import (
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received request")
-	w.Write([]byte(go_hello_world.HelloWorld() + " Bazoku is cool!"))
+	w.Write([]byte(go_hello_world.HelloWorld()))
 }
 
 func getPort() string {
